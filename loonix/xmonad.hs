5502ce045660d232bf67717337c8159d1b625cca
Milan Santosi <milan.santosi@gmail.com>
2010-08-25 02:54:26 +0200

    java cheat fix

--- loonix/xmonad.hs
+++ loonix/xmonad.hs
@@ -2,15 +2,21 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig
 import XMonad.Hooks.ManageHelpers
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import XMonad.Layout.Tabbed
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.NoBorders
+
 import XMonad.Prompt
+import XMonad.Prompt.Shell
 
 myPromptConfig :: XPConfig
 myPromptConfig = defaultXPConfig {
-                   font = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-*"
+                   font = "-*-terminus-*-*-*-*-14-*-*-*-*-*-*-*"
                  , bgColor = "#111111"
                  , fgColor = "#FFFFFF"
                  , bgHLight = "#111111"
@@ -22,13 +28,19 @@ myPromptConfig = defaultXPConfig {
                  , defaultText = []
                  }
 
+myManageHook = manageDocks <+> composeAll
+    [ className =? "stalonetray" --> doIgnore]
+
+
 main = do
-    xmproc <- spawnPipe "/home/milan/.cabal/bin/xmobar /home/milan/.xmobarrc"
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/milan/.xmobarrc"
     xmonad $ defaultConfig
-        { manageHook = composeOne [ isFullscreen -?> doFullFloat ] <+>  manageDocks <+> manageHook defaultConfig
+        { manageHook = manageHook defaultConfig <+> myManageHook <+> composeOne [isFullscreen -?> doFullFloat]
         , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
+        , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
         , terminal = "urxvt"
-        , focusedBorderColor = "blue"  -- "#ff0000" don't use hex, not <24 bit safe        
+        , normalBorderColor = "white"
+        , focusedBorderColor = "green"
         , logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
@@ -37,8 +49,14 @@ main = do
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((mod4Mask .|. shiftMask, xK_Return), spawn "urxvt")
+        , ((mod4Mask .|. shiftMask, xK_n), spawn "emacsclient -c")
         , ((mod4Mask, xK_p), spawn "exe=`dmenu_path | dmenu -b` && eval \"exec $exe\"")
+        , ((mod4Mask, xK_x), shellPrompt defaultXPConfig)
+        , ((mod4Mask, xK_b), sendMessage ToggleStruts)
+        , ((mod4Mask .|. shiftMask, xK_F9), spawn "sudo poweroff")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         ]
 
+
+ 
